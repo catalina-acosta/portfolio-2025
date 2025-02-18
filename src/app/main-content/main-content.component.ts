4aa1960b9c4b232569428ee8f2bbf07214a9ce12
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-18 14:23:23 +0100

    added navbar component and navbar style. Also first draft responsive navbar and hero

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroComponent],
+  imports: [HeroComponent, NavbarComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
