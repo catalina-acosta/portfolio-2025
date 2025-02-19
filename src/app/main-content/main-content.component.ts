fbf373e02fa73595e92d07fdcb88d0088196bbdf
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-19 13:14:40 +0100

    added graphics for sections projects, skills and about + created projects component and it's html structure

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,10 +3,12 @@ import { HeroComponent } from './hero/hero.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from "./about/about.component";
 import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroComponent, NavbarComponent, AboutComponent, SkillsComponent],
+  imports: [HeroComponent, NavbarComponent, AboutComponent, 
+            SkillsComponent, ProjectsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
