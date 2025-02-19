5d56610d33b51ca6abf0a7ad3a50690c2e3d8cf9
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-19 13:24:31 +0100

    References component created and added basic html structure

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from "./about/about.component";
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ReferencesComponent } from './references/references.component';
 
 @Component({
   selector: 'app-main-content',
   imports: [HeroComponent, NavbarComponent, AboutComponent, 
-            SkillsComponent, ProjectsComponent],
+            SkillsComponent, ProjectsComponent, ReferencesComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
