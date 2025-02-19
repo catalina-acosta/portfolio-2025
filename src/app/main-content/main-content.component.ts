5a848c5b75f6e28811ca4bd841511eb5983a9d71
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-19 11:41:26 +0100

    created about component and added html structure to this section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroComponent, NavbarComponent],
+  imports: [HeroComponent, NavbarComponent, AboutComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
