3a90bd925571f791f1dfd7584aa36519d593c9c1
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-19 10:15:49 +0100

    added onMouseEnter and onMouseLeave to footer, to change icons on hover

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  hoveredIcon: string = '';
 
+  onMouseEnter(icon: string) {
+    this.hoveredIcon = icon;
+  }
+
+  onMouseLeave(icon: string) {
+    this.hoveredIcon = '';
+  }
 }
