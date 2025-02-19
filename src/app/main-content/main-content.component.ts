89edc1d9eacd7a5a5a9bf1d7be6218aea6e7ff78
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-19 13:40:09 +0100

    Contact component created + added html structure

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,11 +5,13 @@ import { AboutComponent } from "./about/about.component";
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   imports: [HeroComponent, NavbarComponent, AboutComponent, 
-            SkillsComponent, ProjectsComponent, ReferencesComponent],
+            SkillsComponent, ProjectsComponent, ReferencesComponent,
+            ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
