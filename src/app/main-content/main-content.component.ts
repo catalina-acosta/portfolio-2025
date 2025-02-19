6da8593f715c730b04e462bfc0d7ff9bc33573e9
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-19 11:49:34 +0100

    skills component created. Added html structure to this section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from "./about/about.component";
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroComponent, NavbarComponent, AboutComponent],
+  imports: [HeroComponent, NavbarComponent, AboutComponent, SkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
