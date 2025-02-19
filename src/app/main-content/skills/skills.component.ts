863d00f4b7f2cc359cef09731c5cdd1cce870369
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-19 15:04:00 +0100

    added skillsList array of skills with imgPath and name to render skills dynamically in skills html using @for.

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skillsList:{name: string; imgPath: string}[] = [
+    {name: "Angular", imgPath: "./assets/graphics/skills/skill-icons/Angular.svg"},
+    {name: "TypeScript", imgPath: "./assets/graphics/skills/skill-icons/Typescript.svg"},
+    {name: "JavaScript", imgPath: "./assets/graphics/skills/skill-icons/Javascript.svg"},
+    {name: "HTML", imgPath: "./assets/graphics/skills/skill-icons/html.svg"},
+    {name: "CSS", imgPath: "./assets/graphics/skills/skill-icons/css.svg"},
+    {name: "Rest-API", imgPath: "./assets/graphics/skills/skill-icons/Api.svg"},
+    {name: "Firebase", imgPath: "./assets/graphics/skills/skill-icons/Firebase.svg"},
+    {name: "GIT", imgPath: "./assets/graphics/skills/skill-icons/git.svg"},
+    {name: "Material Design", imgPath: "./assets/graphics/skills/skill-icons/MaterialDesign.svg"},
+    {name: "Scrum", imgPath: "./assets/graphics/skills/skill-icons/scrum.svg"},
+  ]
 }
