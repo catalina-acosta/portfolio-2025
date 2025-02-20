2067edecbdb98e72c5cb32baa0c2aa4da20b664b
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-20 10:35:12 +0100

    added projectList to ProjectsComponents with an array of objects that contain all information of each project and to be able to render them dynamically

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -7,5 +7,95 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  projectList:{
+    title: string; 
+    content: { sectionTitle: string; text: string}[];
+    technologies: string[];
+    img: string;
+  }[] = [
+    {
+      title: "Join",
+      content: [
+        {
+          sectionTitle: "About the project",
+          text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. "
+        },
+        {
+          sectionTitle: "How I have organized my work process",
+          text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. "
+        },
+        {
+          sectionTitle: "My group work experience            ",
+          text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. "
+        },
+      ],
+      technologies: ["./assets/graphics/projects/technologies/Angular (1).svg", "./assets/graphics/projects/technologies/Typescript.svg", 
+                      "./assets/graphics/projects/technologies/html (1).svg", "./assets/graphics/projects/technologies/css (2).svg"
+                    ],
+      img: "./assets/graphics/projects/Join.png" 
+    },
+    {
+      title: "Sharkie ",
+      content: [
+        {
+          sectionTitle: "About the project",          
+          text: "A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale."
+        },
+        {
+          sectionTitle: "How I have organized my work process",
+          text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. "
+        },
+        {
+          sectionTitle: "What have I learnt ",
+          text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. "
+        },
+      ],
+      technologies: ["./assets/graphics/projects/technologies/Angular (1).svg", "./assets/graphics/projects/technologies/Typescript.svg", 
+                      "./assets/graphics/projects/technologies/html (1).svg", "./assets/graphics/projects/technologies/css (2).svg"
+                    ],
+      img: "./assets/graphics/projects/sharkie.png" 
+    },
+    {
+      title: "Join",
+      content: [
+        {
+          sectionTitle: "About the project",
+          text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. "
+        },
+        {
+          sectionTitle: "How I have organized my work process",
+          text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. "
+        },
+        {
+          sectionTitle: "My group work experience            ",
+          text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. "
+        },
+      ],
+      technologies: ["./assets/graphics/projects/technologies/Angular (1).svg", "./assets/graphics/projects/technologies/Typescript.svg", 
+                      "./assets/graphics/projects/technologies/html (1).svg", "./assets/graphics/projects/technologies/css (2).svg"
+                    ],
+      img: "./assets/graphics/projects/Join.png"  
+    },
+    {
+      title: "Join",
+      content: [
+        {
+          sectionTitle: "About the project",
+          text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. "
+        },
+        {
+          sectionTitle: "How I have organized my work process",
+          text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. "
+        },
+        {
+          sectionTitle: "My group work experience            ",
+          text: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. "
+        },
+      ],
+      technologies: ["./assets/graphics/projects/technologies/Angular (1).svg", "./assets/graphics/projects/technologies/Typescript.svg", 
+                      "./assets/graphics/projects/technologies/html (1).svg", "./assets/graphics/projects/technologies/css (2).svg"
+                    ],
+      img: "./assets/graphics/projects/Join.png"
+    },
+  ]
 }
