afa19f746d304c151272d20079f0141cc6d65dee
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-20 14:57:36 +0100

    added refList to ReferencesComponent.ts and implemented @for in the html file to render the references dynamically. Also componend styling for desktop done

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
-
+  refList: {name: string; project: string; refText: string; linkein: string}[] = [
+    {name: "Caryen Song", 
+      project: "Join", 
+      refText: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure illum magnam nisi facilis amet laudantium dolor esse necessitatibus! Ex, sed assumenda numquam doloribus placeat quidem voluptatem illum. debitis doloremque molestias.",
+      linkein: "#"
+    },
+    {name: "Caryen Song", 
+      project: "Join", 
+      refText: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure illum magnam nisi facilis amet laudantium dolor esse necessitatibus! Ex, sed assumenda numquam doloribus placeat quidem voluptatem illum. debitis doloremque molestias.",
+      linkein: "#"
+    },
+    {name: "Caryen Song", 
+      project: "Join", 
+      refText: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure illum magnam nisi facilis amet laudantium dolor esse necessitatibus! Ex, sed assumenda numquam doloribus placeat quidem voluptatem illum. debitis doloremque molestias.",
+      linkein: "#"
+    },
+  ]
 }
