c94491c555aec4f303a748929c998170b7cf9d24
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-21 12:55:21 +0100

    header sticks to the top when scrolled down

--- src/app/main-content/navbar/navbar.component.ts
+++ src/app/main-content/navbar/navbar.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
