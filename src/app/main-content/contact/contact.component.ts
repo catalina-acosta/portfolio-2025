4c8e5ecc6a4bafd04396e267a2fdcc854d063fa9
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-21 09:14:31 +0100

    created component ContactForm and imported it into Contact. Form logic is implemented and test email working. First styling of contact form done

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from "./contact-form/contact-form.component";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  
 }
