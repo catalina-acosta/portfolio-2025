14e7fc3f3e5f5f88733083d1bbd2337f520aa752
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-26 21:25:11 +0100

    added impressum html structure, translation json element to i18n files and styled the component.

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../main-content/navbar/navbar.component';
+import {TranslatePipe} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  imports: [RouterModule, NavbarComponent, TranslatePipe],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
