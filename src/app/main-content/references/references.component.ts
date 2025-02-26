3165b16756038c1ca937607c9f851a149bb271e6
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-26 15:44:19 +0100

    added translate pipes to contact form and it works

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,29 +1,30 @@
 import { Component } from '@angular/core';
+import {TranslatePipe} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-references',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
-  refList: {name: string; project: string; refText: string; linkein: string; hovered: boolean}[] = [
+  refList: {name: string; project: string; refText: string; linkedin: string; hovered: boolean}[] = [
     {name: "Caryen Song", 
       project: "Join", 
       refText: "‘’Catalina had to develop, format and deliver content in collaboration with the team members. She is a reliable and friendly person.’’",
-      linkein: "#",
+      linkedin: "#",
       hovered: false,
     },
     {name: "Caryen Song", 
       project: "Join", 
-      refText: "‘’Catalina is a reliable and friendly person. Works in a structured way and write a clear code. I recommend her as a colleague.’’",
-      linkein: "#",
+      refText: 'references.reference1',
+      linkedin: "#",
       hovered: false,
     },
     {name: "Caryen Song", 
       project: "Join", 
       refText: "‘’ She is a trustworthy teamplayer and can cope with the stress of deadlines. Structured work and clear code. ‘’",
-      linkein: "#",
+      linkedin: "#",
       hovered: false,
     },
   ];
