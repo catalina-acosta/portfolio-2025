6924a559b0018ceaf4c7ca9a59e00562aad088ce
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-02-26 20:39:16 +0100

    added active class to language button in navbar

--- src/app/main-content/navbar/navbar.component.ts
+++ src/app/main-content/navbar/navbar.component.ts
@@ -14,6 +14,7 @@ import {
 export class NavbarComponent {
   isclicked: boolean = false;
   activeSection: string = "";
+  currentLanguage: string = "en";
 
   constructor(private translate: TranslateService){}
   
@@ -25,11 +26,13 @@ export class NavbarComponent {
     this.activeSection = link;
   }
 
-  setActiveLanguage() {
-
+  setLanguage(currentLanguage: string) {
+    
   }
+
   changeLanguage(language: string) {
     this.translate.use(language);
+    this.currentLanguage = language;
   }
 
 }
