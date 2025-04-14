486fda0c677c0fe256ea7590b5e469d064dc4753
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-04-14 11:53:35 +0200

    added overflow-x: hidden so that there is not a horizontal scrollbar anymore due to the aos animations

--- src/app/main-content/about/about.component.ts
+++ src/app/main-content/about/about.component.ts
@@ -8,10 +8,10 @@ import {TranslateService, TranslatePipe} from "@ngx-translate/core";
   styleUrl: './about.component.scss'
 })
 export class AboutComponent implements OnInit, AfterViewInit {
+
   constructor(private translate: TranslateService) {}
 
   ngOnInit(): void {
-    // Any initialization logic can go here
   }
 
   ngAfterViewInit(): void {
@@ -19,15 +19,24 @@ export class AboutComponent implements OnInit, AfterViewInit {
   }
 
   startTypingAnimation(): void {
-    this.translate.get(['about.span', 'about.location']).subscribe(translations => {
-      const text = `${translations['about.span']}${translations['about.location']}|`;
+    this.translate.get(['about.typingText1']).subscribe(translations => {
+      const text = `${translations['about.typingText1']}|`;
       const typingTextElement = document.getElementById('typing-text');
+      const typingTextElementBlue = document.getElementById('typing-text-blue');
       // noch eine Span hinzufügen text white 
       let index = 0;
+      // typingTextElementBlue!.innerHTML += `<img class="icon-mobile" src="./assets/graphics/about/remote_mobile.svg" alt="">`
 
       const type = () => {
-        // noch eine condition if text.lenght < 4 reder text in text white span
-        if (index < text.length) { // else if this: 
+        if (index < 5) {
+          if(index == 0) {
+            typingTextElementBlue!.innerHTML += `<img class="icon-mobile" src="./assets/graphics/about/remote_mobile.svg" alt="">`;
+          }
+          typingTextElementBlue!.innerHTML += text.charAt(index);
+          index++;
+          setTimeout(type, 100);
+        }
+        else if (index >= 4 && index < text.length) { // else if this: 
           typingTextElement!.innerHTML += text.charAt(index);
           index++;
           setTimeout(type, 100); // Adjust the speed of typing here
@@ -37,16 +46,25 @@ export class AboutComponent implements OnInit, AfterViewInit {
       };
 
       const untype = () => {
-        if (index > 0) {
+        if (index > 4) {
           typingTextElement!.innerHTML = typingTextElement!.innerHTML.slice(0, -1);
           index--;
           setTimeout(untype, 100); // Adjust the speed of untyping here
-        } else {
+        } else if(index > 0) {
+          typingTextElementBlue!.innerHTML = typingTextElement!.innerHTML.slice(0, -1);
+          index--;
+          setTimeout(untype, 100); // Adjust the speed of untyping here
+        }
+        else {
           setTimeout(type, 1000); // Start typing again after a delay
         }
       }
-
+      // typingTextElementBlue!.innerHTML = "";
       type();
     });
   }
 }
+function type() {
+  throw new Error('Function not implemented.');
+}
+
