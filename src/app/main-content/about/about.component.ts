fda06171852076382d573b50493bc434b11809df
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-04-16 09:30:49 +0200

    typing animation in about section working well. Added textsArray and typeSentence to go through this array and display different more than one sentence

--- src/app/main-content/about/about.component.ts
+++ src/app/main-content/about/about.component.ts
@@ -46,48 +46,62 @@ export class AboutComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   startTypingAnimation(): void {
-    this.translate.get(['about.typingText1']).subscribe(translations => {
-      const text = `${translations['about.typingText1']}|`;
-      const typingTextElement = document.getElementById('typing-text');
-      const typingTextElementBlue = document.getElementById('typing-text-blue');
-      const typingIcon = document.getElementById("typingIcon");
-      this.resetTypingAnimation();
+    this.translate.get(['about.typingText1', 'about.typingText2']).subscribe(translations => {
+      const textsArray = [`${translations['about.typingText1']}|`, `${translations['about.typingText2']}|`];
+        const typingTextElement = document.getElementById('typing-text');
+        const typingTextElementBlue = document.getElementById('typing-text-blue');
+        const typingIcon = document.getElementById("typingIcon");
+        let currentIndex = 0; // Track the current sentence being typed
 
-      let index = 0;
-
-      const type = () => {
-        if (index < 5) {
-          if (index === 0) {
-            typingIcon!.innerHTML += `<img class="icon-desktop" src="./assets/graphics/about/Group 60.svg" alt="">`;
+        const typeSentence = () => {
+          if (currentIndex >= textsArray.length) {
+            currentIndex = 0; // Restart the animation from the first sentence
           }
-          typingTextElementBlue!.innerHTML += text.charAt(index);
-          index++;
-          this.typingTimeout = setTimeout(type, 100);
-        } else if (index >= 4 && index < text.length) {
-          typingTextElement!.innerHTML += text.charAt(index);
-          index++;
-          this.typingTimeout = setTimeout(type, 100);
-        } else {
-          this.untypingTimeout = setTimeout(untype, 1000);
-        }
+    
+          const text = textsArray[currentIndex];
+        
+        
+        this.resetTypingAnimation();
+  
+        let index = 0;
+  
+        const type = () => {
+          if (index < 5) {
+            if (index === 0) {
+              typingIcon!.innerHTML += currentIndex === 0 ? `<img class="icon-desktop" src="./assets/graphics/about/Group 60.svg" alt="">` : `<img class="icon-desktop" src="./assets/graphics/about/Icon Remote.svg" alt="">` ;
+            }
+            typingTextElementBlue!.innerHTML += text.charAt(index);
+            index++;
+            this.typingTimeout = setTimeout(type, 100);
+          } else if (index >= 4 && index < text.length) {
+            typingTextElement!.innerHTML += text.charAt(index);
+            index++;
+            this.typingTimeout = setTimeout(type, 100);
+          } else {
+            this.untypingTimeout = setTimeout(untype, 1000);
+          }
+        };
+  
+        const untype = () => {
+          if (index > 4) {
+            typingTextElement!.innerHTML = typingTextElement!.innerHTML.slice(0, -1);
+            index--;
+            this.untypingTimeout = setTimeout(untype, 100);
+          } else if (index > 0) {
+            typingTextElementBlue!.innerHTML = typingTextElementBlue!.innerHTML.slice(0, -1);
+            index--;
+            this.untypingTimeout = setTimeout(untype, 100);
+          } else {
+            typingIcon!.innerHTML = "";
+            currentIndex++;
+            this.typingTimeout = setTimeout(typeSentence, 1000);
+          }
+        };
+  
+        type();
       };
 
-      const untype = () => {
-        if (index > 4) {
-          typingTextElement!.innerHTML = typingTextElement!.innerHTML.slice(0, -1);
-          index--;
-          this.untypingTimeout = setTimeout(untype, 100);
-        } else if (index > 0) {
-          typingTextElementBlue!.innerHTML = typingTextElementBlue!.innerHTML.slice(0, -1);
-          index--;
-          this.untypingTimeout = setTimeout(untype, 100);
-        } else {
-          typingIcon!.innerHTML = "";
-          this.typingTimeout = setTimeout(type, 1000);
-        }
-      };
-
-      type();
+      typeSentence();
     });
   }
 }
